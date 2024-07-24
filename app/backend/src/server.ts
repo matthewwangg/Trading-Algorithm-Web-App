59b8310dba6fb2d3ee677be236d76499905025cf
Matt Wang <matthewianwang@gmail.com>
2024-07-23 21:59:18 -0400

    Added proxy endpoint

--- app/backend/src/server.ts
+++ app/backend/src/server.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
+import axios, { AxiosRequestConfig, Method } from 'axios';
+import { AxiosRequestHeaders } from 'axios';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -21,6 +23,29 @@ app.get('/predictions', (req: Request, res: Response) => {
 
 app.get('/about', (req: Request, res: Response) => {
     res.send('Welcome to the About Page');
+});
+
+app.all('/proxy/*', async (req: Request, res: Response) => {
+    const url = `http://django-api-url${req.path.replace('/proxy', '')}`;
+
+    try {
+        const config: AxiosRequestConfig = {
+            method: req.method as Method,
+            url: url,
+            data: req.body,
+            headers: req.headers as AxiosRequestHeaders,
+        };
+
+        const response = await axios(config);
+
+        res.status(response.status).send(response.data);
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            res.status(error.response?.status || 500).send(error.message);
+        } else {
+            res.status(500).send('Unexpected error');
+        }
+    }
 });
 
 app.listen(PORT, () => {
