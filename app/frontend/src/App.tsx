892c7bedf3d63cf152c7a55038a414ef9fa4a3d1
Matt Wang <matthewianwang@gmail.com>
2024-08-14 18:38:29 -0400

    Added profile page template

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -4,15 +4,17 @@ import HomePage from './pages/HomePage';
 import MetricsPage from './pages/MetricsPage';
 import PredictionsPage from './pages/PredictionsPage';
 import AboutPage from './pages/AboutPage';
+import ProfilePage from "./pages/ProfilePage";
 
 const App: React.FC = () => {
   return (
       <Router>
         <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/metrics" element={<MetricsPage />} />
-          <Route path="/predictions" element={<PredictionsPage />} />
-          <Route path="/about" element={<AboutPage />} />
+            <Route path="/profile" element={<ProfilePage/>} />
+            <Route path="/" element={<HomePage />} />
+            <Route path="/metrics" element={<MetricsPage />} />
+            <Route path="/predictions" element={<PredictionsPage />} />
+            <Route path="/about" element={<AboutPage />} />
         </Routes>
       </Router>
   );
---
+++ app/frontend/src/pages/ProfilePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles.css';
+
+const ProfilePage: React.FC = () => {
+    return (
+        <div className="profile-page-container">
+
+        </div>
+    );
+};
+
+export default ProfilePage;
