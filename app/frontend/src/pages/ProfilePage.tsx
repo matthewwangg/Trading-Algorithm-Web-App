8587a08d48600a74b66d2658d58741fc9a2946bb
Matt Wang <matthewianwang@gmail.com>
2024-08-23 12:21:19 -0400

    Added template components to profile page

--- app/frontend/src/pages/ProfilePage.tsx
+++ app/frontend/src/pages/ProfilePage.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import '../styles.css';
+import Navbar from "../components/Navbar";
+import ProfileCard from "../components/ProfileCard";
 
 const ProfilePage: React.FC = () => {
     return (
         <div className="profile-page-container">
-
+            <Navbar/>
+            <ProfileCard
+                name="Matthew Wang"
+                role1="Computer Science"
+                role2="AI/ML Engineering Intern"
+                description="Incoming CS Senior @ ASU"
+                contactText="Contact"
+                githubText="GitHub"
+            />
         </div>
     );
 };
